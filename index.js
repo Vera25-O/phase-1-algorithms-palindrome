4dd55dd381a5c20adafca6c9c44e6c190ac11a37
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-06-16 14:46:33 +0300

    index.js

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 function isPalindrome(word) {
+  let x = word.length;
+  for (let n = 0; n < x/2; n++) {
+   if (word[n] !== word[x - 1 - n]) {
+    return false;
+   }
   // Write your algorithm here
+  return true;
+  }
 }
-
 /* 
   Add your pseudocode here
 */
 
 /*
+we  reverse a string, this time using a for loop to check if the letters are exactly the same on both sides.
+
+Declare a variable with the length of the string.
+Declare a for loop, using half of the length of the string as a reference point.
+Check if each letter is the same as its  equivalent 
   Add written explanation of your solution here
 */
 
